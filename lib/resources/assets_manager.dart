eecb59fedd64ad13239b24a99c55f88366485abc
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2023-10-10 02:11:35 +0300

    added assets

--- lib/resources/assets_manager.dart
+++ lib/resources/assets_manager.dart
@@ -2,9 +2,16 @@ const String imagePath = 'assets/images';
 const String jsonPath = 'assets/json';
 
 class ImageAssets {
+  static const String gym1 = "$jsonPath/gym1.json";
+  static const String gym2 = "$jsonPath/gym2.json";
+  static const String gym3 = "$jsonPath/gym3.json";
+  static const String gym4 = "$jsonPath/gym4.json";
+  static const String medal = "$jsonPath/medal.json";
 }
 
 class JsonAssets {
   //Splash
   static const String splash = "$jsonPath/splash.json";
+  static const String oops = "$jsonPath/oops.json";
+  static const String menu = "$jsonPath/menu.json";
 }
