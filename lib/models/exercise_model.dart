c4daab79b62fad01147f81bef5521b3f9e2cd469
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2023-10-10 04:38:24 +0300

    created workout and exercises models

---
+++ lib/models/exercise_model.dart
@@ -0,0 +1,15 @@
+class Exercise {
+  final int workOutId;
+  final String squat;
+  final String legPress;
+  final String lunge;
+  final String legExtension;
+
+  Exercise({
+    required this.workOutId,
+    required this.squat,
+    required this.legPress,
+    required this.lunge,
+    required this.legExtension,
+  });
+}
---
+++ lib/models/workout_model.dart
@@ -0,0 +1,25 @@
+class WorkOut {
+  final int id;
+  final String imageUrl;
+  final String title;
+  final String trainer;
+  final double time;
+  final int exercises;
+  final int cost;
+  final String description;
+  final String videoUrl;
+  final bool isTopRated;
+
+  WorkOut({
+    required this.id,
+    required this.imageUrl,
+    required this.title,
+    required this.trainer,
+    required this.time,
+    required this.exercises,
+    required this.cost,
+    required this.description,
+    required this.videoUrl,
+    required this.isTopRated,
+  });
+}
