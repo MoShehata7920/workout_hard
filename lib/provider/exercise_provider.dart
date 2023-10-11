04c05ebabf2b460717a5c876b80b0aef1e132f6c
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2023-10-11 03:29:34 +0300

    initialized exercises and workout providers

---
+++ lib/provider/exercise_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:workout_hard/models/exercise_model.dart';
+
+class ExerciseData extends ChangeNotifier {
+  static List<Exercise> getExerciseList() {
+    List<Exercise> exerciseList = [
+      Exercise(
+        workOutId: 0,
+        squat: '3 x 15',
+        legPress: '2 x 8',
+        lunge: '4 x 30',
+        legExtension: '3 x 12',
+      ),
+      Exercise(
+        workOutId: 0,
+        squat: '4 x 15',
+        legPress: '3 x 8',
+        lunge: '5 x 30',
+        legExtension: '3 x 12',
+      ),
+      Exercise(
+        workOutId: 0,
+        squat: '2 x 15',
+        legPress: '4 x 8',
+        lunge: '6 x 30',
+        legExtension: '4 x 12',
+      ),
+      Exercise(
+        workOutId: 0,
+        squat: '3 x 15',
+        legPress: '5 x 8',
+        lunge: '7 x 30',
+        legExtension: '4 x 12',
+      ),
+    ];
+    return exerciseList;
+  }
+}
